d70190773c730ba6763efb7ff20cbf478c6691b0
Imanstr26 <isatriowicaksono@gmail.com>
2026-06-02 11:08:16 +0700

    update main

--- main.c
+++ main.c
@@ -5,7 +5,22 @@
 
 
 int main(){
+	int pilihan;
+	char prefix[31];
 	
+	do{
+		printf("\n1. Mencari Prefix");
+		printf("\n2. Mencari Sinonim");
+		printf("\n0. Keluar\n");
+		printf("Masukan Pilihan: ");
+		scanf("%d", &pilihan);
+		
+		if (pilihan == 1){
+			printf("\nMasukan Prefix yang ingin diinput: ");
+			scanf("%s", prefix);
+		}
+		
+	}while (pilihan != 0);
 	
 	return 0;	
 }
